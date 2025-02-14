b53d577a46900729b273ead0b57d8ffb73e7473c
Yurgish <humenyuriipetrovych@gmail.com>
2025-02-14 16:56:20 +0200

    Add project files.

---
+++ ShipLibrary/FullName.cs
@@ -0,0 +1,32 @@
+﻿namespace ShipLibrary
+{
+    /// <summary>
+    /// Represents a full name with first, middle, and last names.
+    /// </summary>
+    public class FullName
+    {
+
+        public string LastName { get; set; } = "Unknown";
+
+        public string FirstName { get; set; } = "Unknown";
+
+        public string MiddleName { get; set; } = "Unknown";
+
+        public FullName() { }
+
+        public FullName(string lastName, string firstName, string middleName)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+        }
+
+        /// <summary>
+        /// Overrides ToString to return the full name as a string.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {MiddleName}";
+        }
+    }
+}
---
+++ ShipLibrary/Ship.cs
@@ -0,0 +1,62 @@
+﻿namespace ShipLibrary
+{
+    /// <summary>
+    /// Represents the base class for a ship.
+    /// </summary>
+    public class Ship
+    {
+        public string Name { get; set; } = "Unknown Ship";
+
+        public string Type { get; set; } = "Unknown Type";
+
+        public string HomePort { get; set; } = "Unknown Port";
+
+        public double Displacement { get; set; } = 0;
+
+        public FullName CaptainName { get; set; } = new FullName();
+
+        public int CrewCount { get; set; } = 0;
+
+        public DateTime LaunchDate { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// The engine power in kilowatts.
+        /// </summary>
+        public double EnginePower { get; set; } = 0;
+
+        private double _currentSpeed;
+
+        /// <summary>
+        /// The current speed of the ship in knots.
+        /// </summary>
+        public double CurrentSpeed
+        {
+            get => _currentSpeed;
+            set => _currentSpeed = value < 0 ? 0 : value;
+        }
+
+        public Ship() { }
+
+        public Ship(string name, string type, string homePort, double displacement, FullName captainName, int crewCount,
+                    DateTime launchDate, double enginePower)
+        {
+            Name = name;
+            Type = type;
+            HomePort = homePort;
+            Displacement = displacement;
+            CaptainName = captainName;
+            CrewCount = crewCount;
+            LaunchDate = launchDate;
+            EnginePower = enginePower;
+            _currentSpeed = 0;
+        }
+
+        /// <summary>
+        /// Overrides the ToString method to return ship information.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{Name} ({Type}) - Captain: {CaptainName}, Current Speed: {CurrentSpeed} knots.";
+        }
+    }
+}
